31406ffc52179125c4d493a27862fa771990d9bd
pedroGomes231 <pedro.g.oliveira15@aluno.senai.br>
2026-03-09 16:40:48 -0300

    build: atualizando python 3.14.3

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from api-series!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_rood():
+    return {"mensagem": "Ola, mundo"}
+
+@app.get("/itens/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/soma/{a}/{b}")
+def soma(a: int, b: int):
+    return {"resultado": a + b}
